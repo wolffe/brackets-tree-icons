27bc69aae7b316e40aa293bfc09d9361291bbbf6
Martin Skočík <mskocik@gmail.com>
2017-02-03 18:53:30 +0100

    Move icon to ':before' pseudoelement

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# brackets-tree-icons
-Extended version of drewbkoch's [Brackets File Icons](https://github.com/drewbkoch/Brackets-File-Icons) with folder icon.
+Brackets Tree Icons
+==============
+This extension adds file icons to the brackets file tree. Based on [drewbkoch](https://github.com/drewbkoch) and his [Brackets File Icons](https://github.com/drewbkoch/Brackets-File-Icons) project, which is based on [ivogabe's](https://github.com/ivogabe)[Brackets-Icons](https://github.com/ivogabe/Brackets-Icons) project. Thank to both of you.
+
+Screenshots
+--------------
+![Screenshot](https://raw.githubusercontent.com/mskocik/brackets-tree-icons/master/screenshots/screenshot1.jpg)
+
+### Folder icon included
+![Screenshot](https://raw.githubusercontent.com/mskocik/brackets-tree-icons/master/screenshots/folder.jpg)
+
+
+Supported files
+---------------
+
+The following files are supported at the moment:
+
+ - JavaScript
+ - JSON
+ - TypeScript
+ - CoffeeScript
+ - LiveScript
+ - HTML, HTM
+ - SVG
+ - XML
+ - plist
+ - PHP, SQL
+ - Java
+ - CSS, SASS, Less, Stylus
+ - Shell script, Batch, command
+ - Jade, EJS, Dust, Haml
+ - TXT
+ - Log
+ - Markdown
+ - Map
+ - PNG, JPG, JPEG, TIFF, ICO
+ - GIF
+ - MP4, WebM, OGG
+ - MP3, WAV
+ - EOT, TTF, WOFF
+ - GitIgnore, GitModules, GitAttributes
+ - NPMIgnore, ORG (editorconfig.org)
+ - HTAccess, HTPasswd, Conf
+ - YAML
+ - Project, Jscsrc, Jshintrc, Csslintrc, Todo, Classpath
+ - Zip, Rar, 7z, Tgz, Tar, Gz, Bzip
+ - ERB, RB, RDOC
+ - PY, PYC, PYO, PYD
+
+How to install
+--------------
+Open Brackets, and click the extensions button on the right. Search for 'Brackets File Icons' and click install.
+
+What else is in the package?
+----------------------------
+
+Integrates great with [Brackets-git](https://github.com/zaggino/brackets-git) to better display new, modified files in the file tree.
+
+
+License
+-------
+Brackets Tree Icons is licensed under the [MIT license](http://opensource.org/licenses/MIT). The original Brackets Icons & Brackets File Icons is also licensed under the MIT license.
+
+Font Awesome icons by Dave Gandy - http://fontawesome.io
+
+Octicons icons by Github - http://github.com
+
+Pictonic icons by Pictonic - http://pictonic.co
---
+++ main.js
@@ -0,0 +1,189 @@
+define(function (require, exports, module) {
+
+	var fileInfo = {};
+
+	function addIcon(extension, icon, color, size) {
+		fileInfo[extension] = {
+			icon: icon,
+			color: color,
+			size: size
+		};
+	}
+
+	function addAlias(extension, other) {
+		fileInfo[extension] = fileInfo[other];
+	}
+
+	// Folder
+	addIcon('folder', '\uf139', '#ADB9BD', 20);
+
+	// XML
+	addIcon('xml', '\uf05f', '#ff6600');
+	addIcon('plist', '\uf05f', '#5883f9');
+	addIcon('html', '\uf13b', '#d28445');
+	addAlias('htm', 'html');
+
+	// Stylesheets
+	addIcon('css', '\uf13c', '#6a9fb5');
+	addIcon('scss', '\uf13c', '#c6538c');
+	addAlias('sass', 'scss');
+	addIcon('less', '\uf13c', '#3b6bb2');
+	addIcon('styl', '\uf13c', '#b3d107');
+
+	// JavaScript
+	addIcon('js', '\ue097', '#f4bf75');
+//	addIcon('js', '\ue0a6', '#689F63');    node-like icon
+	addIcon('ejs', '\uf05f', '#f4bf75');
+	addIcon('ts', '\uf05f', '#228ed6');
+	addIcon('coffee', '\ue0b3', '#c9905e');
+	addIcon('json', '\uf096', '#F4BF75');
+	addIcon('ls', '\uf269', '#369bd7');
+
+	// Server side
+	addIcon('php', '\ue09a', '#6976c3');
+	addIcon('sql', '\uf096', '#c67f07');
+
+	// Java
+	addIcon('java', '\ue098', '#75b4de');
+	addAlias('class', 'java');
+
+	// Ruby
+	addIcon('rb', '\ue091', '#ba4a32', 15);
+	addAlias('erb',   'rb');
+	addAlias('rdoc',  'rb');
+
+
+	// Python
+	addIcon('py', '\ue09e', '#75b4de');
+	addAlias('pyc',   'py');
+	addAlias('pyo',  'py');
+	addAlias('pyd',  'py');
+
+	// Shell and friends
+	addIcon('sh', '\ue0b7');
+	addIcon('bat', '\ue0b7');
+	addIcon('command', '\ue0b7');
+
+	// Templating
+	addIcon('jade', '\uf13b', '#01dfa5');
+	addIcon('dust', '\uf13b', '#b2270b');
+	addIcon('haml', '\uf13b', '#eaeae1');
+
+	// Images
+	addIcon('png', '\uf147', '#dbb1a9');
+	addIcon('jpg', '\uf147', '#dedfa3');
+	addAlias('jpeg', 'jpg');
+	addIcon('tiff', '\uf147', '#f88b66');
+	addIcon('ico', '\uf147', '#b6d2d1');
+	addIcon('svg', '\uf147', '#c0c5eb');
+
+	addIcon('gif', '\uf012', '#aaecc0');
+
+	// Videos
+	addIcon('mp4', '\uf24d');
+	addAlias('webm', 'mp4');
+	addAlias('ogg', 'mp4');
+
+	// Audio
+	addIcon('mp3', '\uf258');
+	addAlias('wav', 'mp3');
+
+	// Fonts
+	addIcon('ttf', '\uf241', '#fa5656');
+	addIcon('eot', '\uf241', '#fca82b');
+	addIcon('woff', '\uf241', '#fd9be5');
+
+	// Readme
+	addIcon('md', '\uf0c9', '#c36b35');
+	addAlias('markdown', 'md');
+
+	// Git
+	addIcon('gitignore', '\uf084', '#a0422e', 18);
+	addIcon('gitmodules', '\uf020');
+	addIcon('gitattributes', '\uf020');
+
+	// Webservers
+	addIcon('htaccess', '\uf02f');
+	addIcon('htpasswd', '\uf02f');
+	addIcon('conf', '\uf02f');
+
+	// Archive
+	addIcon('zip', '\uf013');
+	addIcon('rar', '\uf013');
+	addIcon('7z', '\uf013');
+	addIcon('tgz', '\uf013');
+	addIcon('tar', '\uf013');
+	addIcon('gz', '\uf013');
+	addIcon('bzip', '\uf013');
+
+	// Settings
+	addIcon('project', '\uf013');
+	addAlias('jscsrc', 'project');
+	addAlias('jshintrc', 'project');
+	addAlias('csslintrc', 'project');
+	addAlias('todo', 'project');
+	addAlias('classpath', 'project');
+
+	// Other text files
+	addIcon('txt', '\uf011');
+	addIcon('log', '\uf011');
+	addIcon('npmignore', '\uf084', '#a0422e', 18);
+	addIcon('yml', '\uf011');
+	addIcon('ls', '\uf011');
+	addIcon('org', '\uf011');
+
+	// Other Files
+	addIcon('map', '\uf203');
+
+
+	var WorkingSetView = brackets.getModule('project/WorkingSetView');
+	var ExtensionUtils = brackets.getModule('utils/ExtensionUtils');
+	var FileTreeView = brackets.getModule('project/FileTreeView');
+	var FileUtils = brackets.getModule('file/FileUtils');
+
+	ExtensionUtils.loadStyleSheet(module, 'styles/style.css');
+
+	var provider = function (entry) {
+		var data = null;
+
+		if (!entry.isFile) {
+//			return;
+			data = fileInfo['folder'];
+			data.margin = {
+				right: '5px',
+				left: '-5px'
+			};
+		} else {
+			var ext = entry.fullPath.substring(entry.fullPath.lastIndexOf('.') + 1) || entry.name.substr(1);
+//			var ext = FileUtils.getSmartFileExtension(entry.fullPath) || entry.name.substr(1);
+			if (fileInfo.hasOwnProperty(ext)) {
+				data = fileInfo[ext];
+			} else {
+				data = fileInfo.txt;
+			}
+		}
+
+		var $new = $('<ins>');
+		$new.attr('data-icon', data.icon);
+//		$new.attr('data-color', data.color);
+//		$new.addClass('jstree-icon file-icon');
+		$new.addClass('file-icon');
+		$new.css({
+			color: data.color,
+			fontSize: (data.size || 16) + 'px'
+		});
+		//folder css
+		if (data.margin) {
+			$new.css({
+				top: '-2px',
+				position: 'relative',
+				marginLeft: data.margin.left,
+				marginRight: data.margin.right
+			});
+		}
+		return $new;
+	};
+
+	FileTreeView.addIconProvider(provider);
+	WorkingSetView.addIconProvider(provider);
+});
